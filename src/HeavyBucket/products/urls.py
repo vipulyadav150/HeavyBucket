1f2a2ee0a9bdf97ae63e59cacc7aee20e2de63f7
vipulyadav150 <vipulyadav150@gmail.com>
2018-08-17 06:48:15 +0530

    Implemented card views for products list and reverse urls to navigate to detail views,added a navbar

--- src/HeavyBucket/products/urls.py
+++ src/HeavyBucket/products/urls.py
@@ -1,5 +1,4 @@
 
-
 from django.conf.urls import url , include
 
 
@@ -14,11 +13,12 @@ from .views import (
         )
 
 
+app_name = 'products'
 
 urlpatterns = [
 
 
-    url(r'^$', ProductListView.as_view()),
+    url(r'^$', ProductListView.as_view(),name='list'),
     url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view(),name = 'detail'),
 
 ]
