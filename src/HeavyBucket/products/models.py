9abf4df58a297bc7de3dbb798ec5936cd7db8920
vipulyadav150 <vipulyadav150@gmail.com>
2018-08-19 07:19:21 +0530

    Implemented and improved search by adding the Tag filter Query in Custom Model Manager

--- src/HeavyBucket/products/models.py
+++ src/HeavyBucket/products/models.py
@@ -32,7 +32,7 @@ class ProductQuerySet(models.query.QuerySet):
         return self.filter(featured=True, active=True)
     def search(self,query):
         lookups = (Q(title__icontains=query) | Q(description__icontains=query) |
-                   Q(price__icontains=query))
+                   Q(price__icontains=query) | Q(tag__title__icontains=query))
         return self.filter(lookups).distinct()
 
 class ProductManager(models.Manager):
