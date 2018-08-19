3f5636eb2cbde3a97c5ecc331244ccf05cdcf1db
vipulyadav150 <vipulyadav150@gmail.com>
2018-08-19 07:10:31 +0530

    Added the tag model for products

--- src/HeavyBucket/products/models.py
+++ src/HeavyBucket/products/models.py
@@ -1,4 +1,5 @@
 import random
+from django.db.models import Q
 import os
 from django.db import models
 from django.db.models.signals import pre_save, post_save
@@ -29,6 +30,10 @@ class ProductQuerySet(models.query.QuerySet):
 
     def featured(self):
         return self.filter(featured=True, active=True)
+    def search(self,query):
+        lookups = (Q(title__icontains=query) | Q(description__icontains=query) |
+                   Q(price__icontains=query))
+        return self.filter(lookups).distinct()
 
 class ProductManager(models.Manager):
     def get_queryset(self):
@@ -45,6 +50,10 @@ class ProductManager(models.Manager):
         if qs.count() == 1:
             return qs.first()
         return None
+
+    def search(self,query):
+        # lookups = Q(title__icontains=query) | Q(description__icontains=query)
+        return self.get_queryset().active().search(query)
 
 
 
