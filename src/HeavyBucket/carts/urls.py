00b43b43fb955671bb51aad4bafc4522a82cd03d
vipulyadav150 <vipulyadav150@gmail.com>
2018-08-24 02:24:45 +0530

    Updated Checkout view and handled the errors occuring for Math operations with Decimal and Float values

--- src/HeavyBucket/carts/urls.py
+++ src/HeavyBucket/carts/urls.py
@@ -1,20 +1,18 @@
-
-from django.conf.urls import url , include
-
-
+from django.conf.urls import url
 from .views import (
-      cart_home,
-    cart_update
-        )
+    cart_home,
+    cart_update,
+    checkout_home
+)
 
 
 app_name = 'cart'
 
 urlpatterns = [
 
-
-    url(r'^$', cart_home,name='home'),
-    url(r'^update/$', cart_update,name = 'update'),
+    url(r'^$', cart_home, name='home'),
+    url(r'^update/$', cart_update, name='update'),
+    url(r'^checkout/$', checkout_home, name='checkout'),
 
 ]
 
