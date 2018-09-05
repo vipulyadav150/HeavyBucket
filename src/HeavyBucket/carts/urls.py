2e4508316f81a76d723e973c46a97bc9360d80f1
vipulyadav150 <vipulyadav150@gmail.com>
2018-09-05 21:04:56 +0530

    Completed the whole checkout process

--- src/HeavyBucket/carts/urls.py
+++ src/HeavyBucket/carts/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from .views import (
     cart_home,
     cart_update,
-    checkout_home
+    checkout_home,
+    checkout_done_view
 )
 
 
@@ -13,7 +14,7 @@ urlpatterns = [
     url(r'^$', cart_home, name='home'),
     url(r'^update/$', cart_update, name='update'),
     url(r'^checkout/$', checkout_home, name='checkout'),
-
+    url(r'^checkout/success/$', checkout_done_view, name='success'),
 ]
 
 
